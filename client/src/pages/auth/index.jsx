8cd981c23d0906537f1aaad9b949499ac1578dcf
Eriyadan Febina <febinaeriyadan@gmail.com>
2025-05-09 14:34:09 +0800

    Update app name in auth

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -44,8 +44,8 @@ function AuthPage() {
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center border-b">
         <Link to={'/'} className="flex items-center justify-center">
-          <GraduationCap className="h-8 w-8 mr-4" />
-          <span className="font-extrabold text-xl">ONLINE LEARN</span>
+          <GraduationCap className="h-8 w-8 mr-4 " />
+          <span className="font-extrabold text-xl">ONLINE LEARNING PLATFORM</span>
         </Link>
       </header>
       
